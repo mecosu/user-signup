8a28fe0073ef713778b40e83ec35f3c7931362c3
mecosu <30613825+mecosu@users.noreply.github.com>
2018-06-18 23:11:18 -0600

    add app.route for signup confirmation page

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True  
 
+@app.route("/signup-confirmation", methods=['POST'])
+def user_signup_confirmation():
+
+    #username = request.form['username']
+    #password = request.form['password']
+    #verify_password = request.form['verify-password']
+    #email = request.form['email']
+
+    return render_template('signup-confirmation.html', username = request.form['username'])
+
 @app.route("/")
 def index():
     encoded_error = request.args.get("error")
