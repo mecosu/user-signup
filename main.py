f57d777f408d4616f1c325ef937fd6049eceb048
mecosu <30613825+mecosu@users.noreply.github.com>
2018-06-20 23:17:12 -0600

    simplified user_signup_confirmation function

--- main.py
+++ main.py
@@ -42,33 +42,31 @@ def verify_email():
 @app.route("/signup-confirmation", methods=['POST'])
 def user_signup_confirmation():
 
-    error_count = 0
+    username = request.form['username']
+    email = request.form['email']
 
     if not verify_username():
         username_error = "Please enter a valid username."
-        error_count += 1
     else: 
         username_error = ""
     if not verify_password_length():
         password_valid_error = "Please enter a valid password."
-        error_count += 1
     else:
         password_valid_error = ""
     if not verify_passwords_match():
         password_match_error = "Passwords do not match."
-        error_count += 1
     else:
         password_match_error = ""
     if not verify_email():
         email_error = "Please enter a valid email address."
-        error_count += 1
     else:
         email_error = ""
-    if error_count > 0:
+
+    if not username_error and not password_valid_error and not password_match_error and not email_error:
+        return render_template('signup-confirmation.html', username = request.form['username'], password = request.form['password'], verify_password = request.form['verify-password'], email = request.form['email'] )
+    else:
         return render_template('edit.html', username_error = username_error, password_valid_error = password_valid_error, password_match_error = password_match_error, email_error = email_error)
     
-    return render_template('signup-confirmation.html', username = request.form['username'], password = request.form['password'], verify_password = request.form['verify-password'], email = request.form['email'] )
-
 @app.route("/")
 def index():
     encoded_error = request.args.get("error")
